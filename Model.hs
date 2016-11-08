f28761667f73e0431f13ff5f7217a3f96c1c5482
paolino <paolo.veronelli@gmail.com>
2016-11-08 08:29:36 +0100

    implemented modify on the Model

--- Model.hs
+++ Model.hs
@@ -1,59 +1,84 @@
-{-# LANGUAGE TemplateHaskell, DeriveFunctor, ViewPatterns, RankNTypes, ImpredicativeTypes #-}
+{-# LANGUAGE TemplateHaskell, DeriveFunctor, ViewPatterns,OverloadedStrings #-}
+-- | A graph model for shared client server knowledge (state)
+-- We use the expressiveness of fgl, without edge labels.
+-- Totally type unsafe. In particular there is no control on the existance of
+-- non sensical edges
+--
+module Model (Knowledge,Selector,Operation(..),Target(..),query,modify) where
 
-import Data.Graph.Inductive
 import Data.Text (Text)
-import Data.String
-import Control.Lens.TH
-import Control.Lens
-import Data.Tuple
+import Data.Maybe (fromJust)
+import Control.Lens.TH (makeLenses, makePrisms)
+import Control.Lens (clonePrism, APrism',re, view, toListOf)
+import Data.Tuple (swap)
 import qualified Data.Map as M
-import Data.Maybe
-import Data.Monoid
+import Data.Graph.Inductive (Gr,LNode,Node,LEdge,lab,match,newNodes,
+  delNode,delEdge,insNode,insEdge,mkGraph)
 
+data Knowledge a = Knowledge {
+  _graph :: Gr a (), -- only considering vertices, knowledge is spreaded on
+  _concepts :: M.Map a Node -- internal resolution on names to Node
+                             } deriving Show
 
-data Concept = User Text | Role Text | Permission Text deriving (Show,Eq,Ord)
+makeLenses ''Knowledge
 
-makePrisms ''Concept
+type Selector a = APrism' a Text
 
-data Knowledge = Knowledge {
-  _graph :: Gr Concept (),
-  _concepts :: M.Map Concept Node
-  }
-
-type Selector = APrism' Concept Text
-
-knowledge  :: [LNode Concept] -> [LEdge ()] -> Knowledge
+-- | build a knowledge from vertices and edges
+knowledge  :: Ord a => [LNode a] -> [LEdge ()] -> Knowledge a
 knowledge concepts relations = let
   in Knowledge
     (mkGraph concepts relations)
     (M.fromList $ map swap concepts)
 
-query0 :: Selector -> Concept -> Knowledge -> [Text]
-query0 t c k = toListOf (traverse . clonePrism t) $ let
-    (Just (map snd -> ys,_,_, map snd -> xs),_) = match (concepts k M.! c) (graph k)
-    resolve = map (fromJust . lab (graph k))
+query0 :: Ord a => Selector a -> a -> Knowledge a -> [Text]
+query0 t c (Knowledge g cs) = toListOf (traverse . clonePrism t) $ let
+    (Just (map snd -> ys,_,_, map snd -> xs),_) = match (cs M.! c) g
+    resolve = map (fromJust . lab g)
   in (resolve xs ++ resolve ys)
 
-query :: [Selector] -> Concept -> Knowledge -> [[Concept]]
+-- | query is performed on a bfs base. Each selector retrieve a level of knowledge
+-- from the starting vertex. Each selector filter out the result by applying the
+--  Prism as a   filter (more, much more to understand, before saying)
+query :: Ord a => [Selector a] -> a -> Knowledge a -> [[a]]
 query [] _ _ = []
 query (t:ts)  c k = let
   xs = map (view (re (clonePrism t))) . query0 t c $ k
   in xs:concatMap (\c -> query ts c k) xs
 
-data Target =
-  Link Concept Concept
-  Vertex Concept
+-- |  target of a modification
+data Target a = Link a a | Vertex a
 
-data Operation = Insert Target | Delete Target
+-- | operation to be performed on a target
+data Operation a = Insert (Target a) | Delete (Target a)
 
-modify :: Operation -> Knowledge -> Knowledge
+-- | interpret a modification as a change of knowledge
+modify :: Ord a => Operation a -> Knowledge a -> Knowledge a
+modify (Insert (Vertex c)) k@(Knowledge g cs) = case M.lookup c cs of
+        Nothing -> let
+                [n] = newNodes 1 g
+             in Knowledge  (insNode (n,c) g) (M.insert c n cs)
+        _ -> k -- failing silently
 
-    {-
-insert :: Concept -> Concept -> Knowledge -> Knowledge
-insert c1 c2 k = let
+modify (Delete (Vertex c)) k@(Knowledge g cs)  = case M.lookup c cs of
+        Just n -> Knowledge  (delNode n g) (M.delete c cs)
+        _ -> k -- failing silently
 
-delete :: Concept ->
--}
+modify (Insert (Link c1 c2)) k@(Knowledge g cs) =
+          case (,) <$> M.lookup c1 cs <*> M.lookup c2 cs of
+            Nothing -> k
+            Just (n1,n2) -> Knowledge (insEdge (n1,n2,()) g) cs
+
+modify (Delete (Link c1 c2)) k@(Knowledge g cs) = case (,) <$> M.lookup c1 cs <*> M.lookup c2 cs of
+            Nothing -> k
+            Just (n1,n2) -> Knowledge (delEdge (n1,n2) g) cs
+
+
+----------------- example -------------------------------------------
+data Concept = User Text | Role Text | Permission Text deriving (Show,Eq,Ord)
+
+makePrisms ''Concept
+
 cs :: [LNode Concept]
 cs =  [
                 (1,Role "Account administrator"),
@@ -98,5 +123,5 @@ rs = [          (1,9, ()),
                 (3,5, ()),
                 (3,8, ())
                 ]
-
-k = knowledge cs rs
+example :: Knowledge Concept
+example = knowledge cs rs
